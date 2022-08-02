f1bbcf10427069b0b6da50899211691588d57f87
vignesh.p <vignesh.p@QT-INBG-LT0402.QUALITEST.LOCAL>
2022-08-02 10:40:08 +0530

    4

--- Project_Mobile/src/mobileworld/SignIn.java
+++ Project_Mobile/src/mobileworld/SignIn.java
@@ -3,6 +3,7 @@ package mobileworld;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -10,6 +11,8 @@ public class SignIn extends Webdriver {
 
      WebDriver driver;
 	
+    @Test
+     
 	public  void signIn()  {
 		// TODO Auto-generated method stub
 		
--- Project_Mobile/src/mobileworld/SignUp.java
+++ Project_Mobile/src/mobileworld/SignUp.java
@@ -1,18 +1,39 @@
 package mobileworld;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SignUp {
 
-	WebDriver driver;
 	
-	public  void signUp() throws InterruptedException  {
+	
+	public  static void main(String[] args) throws InterruptedException  {
 		// TODO Auto-generated method stub
 		
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://mobileworld.azurewebsites.net/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+		
+		driver.findElement(By.className("btn")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.linkText("Sign up")).click();
+		driver.findElement(By.id("myName")).sendKeys("Vignesh");
+		driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Viki");
+		driver.findElement(By.xpath("//input[@placeholder='Enter Email']")).sendKeys("viki123@gmail.com");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("XXXXXXXX");
+		driver.findElement(By.xpath("//input[@type='date']")).sendKeys("04-03-1998");
+		driver.findElement(By.xpath("//input[@type='number']")).sendKeys("9876543210");
+		
+		
+		
+		
 		
 
 	}
