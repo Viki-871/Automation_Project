4d31cdde8b093a0c34e09c5867e74c82dece8d66
vignesh.p <vignesh.p@QT-INBG-LT0402.QUALITEST.LOCAL>
2022-08-02 17:45:30 +0530

    22

--- Project_Mobile/src/test/java/dataProvider/DP001.java
+++ Project_Mobile/src/test/java/dataProvider/DP001.java
@@ -1,14 +1,17 @@
 package dataProvider;
 
+import org.testng.annotations.DataProvider;
+
 public class DP001 {
 
+	@DataProvider(name="mobileData")
 	public static String[][] getData() {
 		
+		String[][] testData = new String[2][4];
 		
 		
 		
-		
-		return null;
+		return testData;
 		
 
 		
